105170fc2b9792a6eb639fccf4e9e1ff68f07c87
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-05-11 08:54:19 +0200

    Plugin JavaPackager - Ejecutar un comando cualquiera

--- src/org/miPlugin/JavaPackagerPlugin.java
+++ src/org/miPlugin/JavaPackagerPlugin.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.miPlugin;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(
@@ -24,8 +23,35 @@ import org.openide.util.NbBundle.Messages;
 @Messages("CTL_JavaPackagerPlugin=JavaPackager")
 public final class JavaPackagerPlugin implements ActionListener {
 
+    /**
+     * Funcion actionPerformed donde se llevara a cabo la accion del plugin.
+     * @param e 
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO implement action body
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec("touch hola.txt");
+ 
+            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+ 
+            String line=null;
+ 
+            while((line=input.readLine()) != null) {
+                System.out.println(line);
+            }
+ 
+            int exitVal = 0;
+            try {
+                exitVal = pr.waitFor();
+            } catch (InterruptedException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            System.out.println("Exited with error code "+exitVal);
+ 
+        } catch(IOException ex) {
+            System.out.println(e.toString());
+            ex.printStackTrace();
+        }
     }
 }
